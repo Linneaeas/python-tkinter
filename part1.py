b378a26c29d47850f769bb1f0db18ff834e27f87
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-10 15:39:57 +0200

    label

--- part1.py
+++ part1.py
@@ -5,5 +5,9 @@ root=tk.Tk()
 root.title("Basic GUI window")
 root.geometry("300x350")
 
+# Label
+label=tk.Label(root, text="This is a label")
+label.pack(pady=10)
+
 # Run the application
 root.mainloop()
