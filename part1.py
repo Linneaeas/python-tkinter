a92d76a86e1cdcd92219744ed9259882bf0d1cf3
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-10 15:44:01 +0200

    Entry box(Input) Button

--- part1.py
+++ part1.py
@@ -7,7 +7,15 @@ root.geometry("300x350")
 
 # Label
 label=tk.Label(root, text="This is a label")
-label.pack(pady=10)
+label.pack(pady=5)
+
+# Entry Box
+entry = tk.Entry(root)
+entry.pack(pady=5)
+
+# Button
+button = tk.Button(root, text="Click me")
+button.pack(pady=5)
 
 # Run the application
 root.mainloop()
