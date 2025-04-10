ce80ed3e40870dc723741bec439aa483c7941dce
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-10 16:20:44 +0200

    autopep formatter

--- demo.py
+++ demo.py
@@ -1,58 +1,63 @@
 import tkinter as tk
 from tkinter import messagebox
 
-def on_submit():
-    name=entry.get()
-    selected_gender=gender_var.get()
-    subscribed=subscribe_var.get()
-    language=language=language_var.get()
 
-    info=f"Name:{name}\nGender: {selected_gender}\nSubscribed:{subscribed}\nLanguage:{language}"
+def on_submit():
+    name = entry.get()
+    selected_gender = gender_var.get()
+    subscribed = subscribe_var.get()
+    language = language = language_var.get()
+
+    info = f"Name:{name}\nGender: {selected_gender}\nSubscribed:{subscribed}\nLanguage:{language}"
     messagebox.showinfo("Submittedinfo", info)
-    
-    
+
+
 # Main window
-root=tk.Tk()
+root = tk.Tk()
 root.title("Robins Demo")
 root.geometry("350x350")
 
 # Entry widget
-label_name=tk.Label(root, text="Name:")
+label_name = tk.Label(root, text="Name:")
 label_name.pack(pady=5)
 
-entry=tk.Entry(root)
+entry = tk.Entry(root)
 entry.pack()
 
 # Radiobuttons for gender
-label_gender=tk.Label(root, text="Gender:")
+label_gender = tk.Label(root, text="Gender:")
 label_gender.pack(pady=5)
 
-gender_var=tk.StringVar(value="Other")
+gender_var = tk.StringVar(value="Other")
 
-radio_male=tk.Radiobutton(root, text="Male", variable=gender_var, value="Male")
+radio_male = tk.Radiobutton(
+    root, text="Male", variable=gender_var, value="Male")
 radio_male.pack()
-radio_female=tk.Radiobutton(root, text="Female", variable=gender_var, value="Female")
+radio_female = tk.Radiobutton(
+    root, text="Female", variable=gender_var, value="Female")
 radio_female.pack()
-radio_other=tk.Radiobutton(root, text="Other", variable=gender_var, value="Pther")
+radio_other = tk.Radiobutton(
+    root, text="Other", variable=gender_var, value="Pther")
 radio_other.pack()
 
 # Checkbox
-subscribe_var=tk.BooleanVar()
-checkbox=tk.Checkbutton(root, text="Subscribe to our newsletter?", variable=subscribe_var)
+subscribe_var = tk.BooleanVar()
+checkbox = tk.Checkbutton(
+    root, text="Subscribe to our newsletter?", variable=subscribe_var)
 checkbox.pack(pady=5)
 
 # Dropdown menu
-label_language=tk.Label(root, text="Fav language")
+label_language = tk.Label(root, text="Fav language")
 label_language.pack(pady=5)
 
-languages=["JS", "PY", "C#", "C++", "Java"]
-language_var=tk.StringVar(value=languages[1])
+languages = ["JS", "PY", "C#", "C++", "Java"]
+language_var = tk.StringVar(value=languages[1])
 
-dropdown=tk.OptionMenu(root, language_var, *languages)
+dropdown = tk.OptionMenu(root, language_var, *languages)
 dropdown.pack()
 
 # Submit button
-submit_button=tk.Button(root, text="Submit", command=on_submit)
+submit_button = tk.Button(root, text="Submit", command=on_submit)
 submit_button.pack(pady=20)
 
 root.mainloop()
--- part1.py
+++ part1.py
@@ -1,12 +1,12 @@
 import tkinter as tk
 
 # Create the main window
-root=tk.Tk()
+root = tk.Tk()
 root.title("Basic GUI window")
 root.geometry("300x350")
 
 # Label
-label=tk.Label(root, text="This is a label")
+label = tk.Label(root, text="This is a label")
 label.pack(pady=5)
 
 # Entry Box
@@ -18,23 +18,23 @@ button = tk.Button(root, text="Click me")
 button.pack(pady=5)
 
 # Radio Buttons
-radio1=tk.Radiobutton(root, text="Option 1", value=1)
-radio2=tk.Radiobutton(root, text="Option 2", value=2)
+radio1 = tk.Radiobutton(root, text="Option 1", value=1)
+radio2 = tk.Radiobutton(root, text="Option 2", value=2)
 radio1.pack()
 radio2.pack()
 
 # Checkbox
-checkbox=tk.Checkbutton(root, text="Subscribe to newsletter")
+checkbox = tk.Checkbutton(root, text="Subscribe to newsletter")
 checkbox.pack(pady=5)
 
 # Dropdown menu
-options=["JS", "PY", "C#"]
-dropdown_var=tk.StringVar(value=options[0])
+options = ["JS", "PY", "C#"]
+dropdown_var = tk.StringVar(value=options[0])
 
-dropdown_label=tk.Label(root, text="Choose fav language")
+dropdown_label = tk.Label(root, text="Choose fav language")
 dropdown_label.pack(pady=10)
 
-dropdown=tk.OptionMenu(root, dropdown_var, *options)
+dropdown = tk.OptionMenu(root, dropdown_var, *options)
 dropdown.pack()
 
 # Run the application
