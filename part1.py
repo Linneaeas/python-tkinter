c9b5fb4d59ca85dd2c6b44698a7cbf8586b20bec
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-10 15:54:48 +0200

    Radiobuttons, Checkbox, OptionMenu(Dropdown)

--- part1.py
+++ part1.py
@@ -17,5 +17,25 @@ entry.pack(pady=5)
 button = tk.Button(root, text="Click me")
 button.pack(pady=5)
 
+# Radio Buttons
+radio1=tk.Radiobutton(root, text="Option 1", value=1)
+radio2=tk.Radiobutton(root, text="Option 2", value=2)
+radio1.pack()
+radio2.pack()
+
+# Checkbox
+checkbox=tk.Checkbutton(root, text="Subscribe to newsletter")
+checkbox.pack(pady=5)
+
+# Dropdown menu
+options=["JS", "PY", "C#"]
+dropdown_var=tk.StringVar(value=options[0])
+
+dropdown_label=tk.Label(root, text="Choose fav language")
+dropdown_label.pack(pady=10)
+
+dropdown=tk.OptionMenu(root, dropdown_var, *options)
+dropdown.pack()
+
 # Run the application
 root.mainloop()
