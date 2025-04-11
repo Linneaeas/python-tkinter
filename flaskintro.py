15becd0ce3d382b8810761462ce18fbfc9f2df45
Leas <127832519+Linneaeas@users.noreply.github.com>
2025-04-11 17:26:25 +0200

    Flask render_template

--- flaskintro.py
+++ flaskintro.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return "Hello world!"
+    return render_template('index.html')
+
+@app.route('/contact')
+def contact():
+    return "<h1>Contact us!</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>This is the about page</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
